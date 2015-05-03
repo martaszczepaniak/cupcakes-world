bf856b46fb8d5abe7760dd220165cb0167da9c12
Marta Szczepaniak <mart.szcz@gmail.com>
2015-05-03 19:26:17 +0000

    Change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 Cupcake.create([{ name: 'Cherry', price: 5 }, 
                 { name: 'Green', price: 10 }, 
                 { name: 'Small', price: 2 }, 
-                { name: 'Heart', price: 9 }])
+                { name: 'Heart', price: 9 },
+                { name: 'Cream', price: 5 },
+                { name: 'White', price: 6 },
+                { name: 'Pink', price: 4 },
+                { name: 'Whipedcream', price: 8 }])
